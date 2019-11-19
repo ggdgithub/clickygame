824bb0c27b76d98c388e36c647a1e5d77be5fbd7
Gabriel de Leon <gabriel.deleon0126@gmail.com>
2019-11-19 03:07:52 -0800

    working game; added images; finally able to start react server

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>CLICKY GAME!</h1>
+      <p>Click on a picture to earn points. If you click on a picture more than once, you lose!</p>
+    </header>
+  );
+}
+
+export default Header;
